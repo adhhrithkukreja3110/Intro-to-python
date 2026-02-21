04233eb0b0d620fecc25835cdefeeb5e836b3be9
adhhrithkukreja3110 <adhhrithkukreja3110@gmail.com>
2026-02-21 17:32:58 +0530

    Add files via upload

---
+++ Project1.py
@@ -0,0 +1,12 @@
+birth_date = input("the date you were born in (DD)")
+
+birth_month = input("the month you were born in (MM)")
+
+birth_year = input("the year you were born in (YYYY)")
+
+birth_day = input("Enter the day of your birth: ")
+
+time = input("enter the time you were born (HH:MM)")
+
+print("Your birth time is", time)
+print("\n Your birth date is", birth_date, "/", birth_month, "/", birth_year)
