cbd388930a1b227114efcdea9d10a6e2475e5a2f
adhhrithkukreja3110 <adhhrithkukreja3110@gmail.com>
2026-03-04 17:23:17 +0530

    Add files via upload

--- Activity3.py
+++ Activity3.py
@@ -1,15 +1,10 @@
-print("Enter Marks Obtained in 4 Subjects:")
-math = int(input("math: "))
-science = int(input("science: "))
-english = int(input("english: "))
-hindi = int(input("hindi: "))
-
-
-
-sum = math + science + english + hindi
-print("The sum of maths,science,english,hindi is: ", sum)
-
-perc = (sum / 400) *100
-
-print(end ="Percentage Marks: ")
-print(perc)
+i = int(input("Enter a number :"))
+if(i < 15):
+    print ("i is smaller than 15")
+    print ("i'm in if block ")
+else:
+    print ("i is greater than 15")
+    print ("i'm in else block")
+    
+    
+    
---
+++ Activity4.py
@@ -0,0 +1,8 @@
+number = int(input("Enter number to check :"))
+print("number to be checked :", number)
+
+if number % 2 == 0 :
+    print("this is an even number")
+
+else:
+    print("Tis is an odd number")
