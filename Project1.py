06edf1e06dda39aebf56c8ac6a6c8013ad4ca2c6
adhhrithkukreja3110 <adhhrithkukreja3110@gmail.com>
2026-03-10 21:07:49 +0530

    Add files via upload

--- Project1.py
+++ Project1.py
@@ -1,2 +1,6 @@
-wish = input("Enter your good wishes: ")
-print("Your good wishes are: ", wish.upper())
+i = (input("Enter A Letter :"))
+
+if (i is 'a' or i is 'b' or i is 'c' or i is 'd' or i is 'e' or i is 'f' or i is 'g' or i is 'h' or i is 'i' or i is 'j' or i is 'k' or i is 'l' or i is 'm' or i is 'n' or i is 'o' or i is 'p' or i is 'q' or i is 'r' or i is 's' or i is 't' or i is 'u' or i is 'v' or i is 'w' or i is 'x' or i is 'y' or i is 'z'):
+    print("It Is an Alphabet")
+else:   print("It Is Not an Alphabet")
+
