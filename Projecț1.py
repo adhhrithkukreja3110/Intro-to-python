2ecc6bd43a38b86b36784c42fc515df5b67a4ae6
adhhrithkukreja3110 <adhhrithkukreja3110@gmail.com>
2026-03-14 10:39:55 +0530

    Add files via upload

---
+++ Projecț1.py
@@ -0,0 +1,5 @@
+i = int(input("Write a Number You Want to Find the Square Root of: "))
+if i < 0:
+    print("Sorry, you can't find the square root of a negative number.")
+else:
+    print("The square root of", i, "is", i ** 0.5)
