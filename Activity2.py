ccde9cc2df548efafc2c4dc628dd67e6ab8a750a
adhhrithkukreja3110 <adhhrithkukreja3110@gmail.com>
2026-03-19 17:01:02 +0530

    Loops

--- Activity2.py
+++ Activity2.py
@@ -1,22 +1,10 @@
-units = int(input("Enter the number of units you have consumed: "))
+string = input("Please enter your own string: ")
 
-if(units < 50):
-    amount = units * 2.60
-    surcharge = 25
-elif(units <= 100):
-    amount = 130 + ((units - 50)*3.25)
-    surcharge = 35
+string2 = ('')
 
-elif(units <= 200):
-    amount = 130 + 162.5 + ((units - 100)*5.26)
-    surcharge = 45
+for i in string:
+    string2 = i + string2
+    print(string2)
 
-
-
-
-else:
-    amount = 130 + 162.5 + 526 + ((units - 200)*7.75)
-    surcharge = 75
-
-total = amount + surcharge
-print("\n Electricity Bill = %.2f" % total)
+print("\nThe original string : " ,string )
+print("The reversed string : " ,string2)
