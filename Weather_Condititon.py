e359be38b02ac88837dae8063527558e589e3734
adhhrithkukreja3110 <adhhrithkukreja3110@gmail.com>
2026-05-14 12:36:04 +0530

    function

---
+++ Weather_Condititon.py
@@ -0,0 +1,9 @@
+def weather_condition():
+    print('The Weather is Pleasant in:', spring)
+    print('The Weather is Same in', autumn)
+
+spring = "autumn"
+autumn = spring
+weather_condition()
+
+
