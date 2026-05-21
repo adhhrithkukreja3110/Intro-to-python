2591a7074eb95cc1fe266013c99bc38a6018cc63
adhhrithkukreja3110 <adhhrithkukreja3110@gmail.com>
2026-05-21 12:27:06 +0530

    Arguments

---
+++ cube_of_the_cube.py
@@ -0,0 +1,13 @@
+
+def cube(number):
+    return number*number*number
+
+
+def by_three(number):
+    if number %3 ==0:
+        return cube(number)
+    else:
+        return False
+    
+print(by_three(900))
+print(by_three(400))
